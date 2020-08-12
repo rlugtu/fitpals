f08ca04bd36ac84bd0c21e7dc636ac32a77786e0
Ryan Lugtu <ryanlugtu@Ryans-MacBook-Pro.local>
2020-08-12 14:38:01 -0400

    adjusted login routes and footer

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -15,7 +15,7 @@ authRouter.post('/login', passport.authenticate('local', {
 
 authRouter.get('/logout', (req, res) => {
     req.logout()
-    res.redirect('back')
+    res.redirect('/auth/login')
 })
 
 module.exports = authRouter
